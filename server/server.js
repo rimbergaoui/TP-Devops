319cb74c335cf56f415e553053399d3b377287e5
rimbergaoui <bergaouirm@gmail.com>
2024-12-11 17:59:18 +0100

    Project4 projet

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+const port = process.env.PORT_SERVER || 9000;
+app.use(
+  cors()
+);
+app.use(express.json());
+app.use(require("./routes/record"));
+app.get("/", function (req, res) {
+  res.send("App is running");
+});
+const dbo = require("./db/conn");
+
+
+dbo.connectToMongoDB(function (error) {
+  if (error) throw error;
+console.log("the server is running");
+});
+app.listen(port, () => {
+  console.log( process.env.MONGO_URI);
+  console.log("Server is running on port: " + port);
+});
